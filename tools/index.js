6636a133874863de436be3a5d21b63a0d052f5a5
kemsakuai <sakurai.kem@gmail.com>
2020-10-21 08:09:50 +0900

    Add tools dir

--- tools/index.js
+++ tools/index.js
@@ -2,36 +2,51 @@ const { Command } = require('commander'); // (normal include)
 const fs = require('fs');
 const path = require('path');
 const userScirptsDirectry = '../userscripts';
+const outputDirectry = './outputs';
+const jsonObject = JSON.parse(fs.readFileSync('./conf/data.json', 'utf8'));
 
 // １ファイルの内容を置換する
 function replaceSyntax(filePath) {
-    console.log(filePath);
-    fs.readFile(filePath, "utf-8", (err, data) => {
-        var result = data.replace(/.*@match.*/g, '@@@match@@@');
-        while(result.split('@@@match@@@').length > 1) {
-            result = result.replace(/.*@@@match@@@\n@@@match@@@\n*/g, '@@@match@@\n');
-        }
-    });
-    console.log(result);
+    let text = fs.readFileSync(filePath, 'utf-8');
+    // --------------
+    // 複数行にわたる@match記述を、@@@match@@@に置換する
+    // -------
+    let result = text.replace(/.*@match.*/g, '@@@match@@@');
+    let resuttArray = result.split('@@@match@@@')
+    if (resuttArray.length  > 1) {
+        result = resuttArray[0] + '@@@match@@@' + resuttArray[resuttArray.length - 1];
+    } else {
+        result = resuttArray[0];
+    }
+    let matchStrings = jsonObject[path.basename(filePath)];
+    let elems = [];
+    for (let elem of matchStrings) {
+        elems.push("// @match" + "    " + elem);
+    }
+    result = result.replace('@@@match@@@', elems.join('\n'));
+    return result;
 }
 
 // ファイル全ての内容を置換する
 function replaceSyntaxAll(dir) {
-    const filenames = fs.readdirSync(
-        dir);
+    const filenames = fs.readdirSync(dir);
     filenames.forEach((filename) => {
-      const fullPath = path.join(dir, filename);
-      replaceSyntax(fullPath);
+        const fullPath = path.join(dir, filename);
+        let result = replaceSyntax(fullPath);
+        const writeFullPath = path.join(outputDirectry, filename);
+        try {
+            fs.writeFileSync(writeFullPath, result);
+        } catch (e) {
+            throw e;
+        }
     });
 }
 
 // コマンドライン引数をcommanderでパースする
 const program = new Command();
 program.parse(process.argv);
-
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-
 if (typeof fullPath === "undefined") {
     replaceSyntaxAll(userScirptsDirectry);
 } else {
